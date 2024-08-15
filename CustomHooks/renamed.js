3c937b7f33d8dcd82e21691525665c2605c856f0
Alok <mishraalok189381@gmail.com>
2024-08-15 20:37:44 +0530

    Initial commit

---
+++ CustomHooks/renamed.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        // Check if currency is defined before making the request
+        if (currency) {
+            fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+                .then(response => response.json())
+                .then(result => setData(result))
+                .catch(error => console.error("Error fetching currency data:", error));
+        }
+    }, [currency]);
+
+    return data;
+}
+
+export default useCurrencyInfo;
